3c65fe45942a4f5f3c092b3322d0484ddcc744fe
osaesm <osamabinesmail@gmail.com>
2019-09-15 17:37:30 -0500

    Updates

---
+++ precache-manifest.ff9d44dadcd1abdfd192df14c0f28e68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5054f329a394ac9a21fa6e4275768c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c98db90f0e8516347e87",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "05f58fe2ee63b0ce005d",
+    "url": "/static/css/main.ad0055f2.chunk.css"
+  },
+  {
+    "revision": "c98db90f0e8516347e87",
+    "url": "/static/js/2.4669fe3c.chunk.js"
+  },
+  {
+    "revision": "05f58fe2ee63b0ce005d",
+    "url": "/static/js/main.5187979f.chunk.js"
+  },
+  {
+    "revision": "2dd97d607b319529ee12",
+    "url": "/static/js/runtime~main.0344d13a.js"
+  }
+]);
